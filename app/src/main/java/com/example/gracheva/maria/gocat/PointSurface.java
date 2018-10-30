772a0db1333dd734b875e1e308efdead717484da
maria-smolina <marija.smolina@gmail.com>
2018-10-30 11:35:49 +0300

    Initial commit

---
+++ app/src/main/java/com/example/gracheva/maria/gocat/PointSurface.java
@@ -0,0 +1,50 @@
+package com.example.gracheva.maria.gocat;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.util.Random;
+
+public class PointSurface extends SurfaceView {
+    public PointSurface(Context context) {
+        super(context);
+        new PointAnimation().start();
+    }
+
+    private class PointAnimation extends Thread {
+        private static final int DELAY = 100;
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    draw();
+                    sleep(DELAY);
+                }
+            } catch (InterruptedException e) {
+                System.out.println("Point animation is interrupted");
+            }
+
+        }
+
+        private void draw() {
+            SurfaceHolder holder = getHolder();
+            Canvas canvas = holder.lockCanvas();
+            if (canvas != null) {
+                canvas.drawColor(Color.BLACK);
+                Paint paint = new Paint();
+                Bitmap bitmap = BitmapFactory.decodeResource(getContext().getResources(), R.drawable.point);
+                int width = getWidth();
+                int height = getHeight();
+                canvas.drawBitmap(bitmap, new Random().nextInt(width - bitmap.getWidth()), new Random().nextInt(height - bitmap.getHeight()), paint);
+                holder.unlockCanvasAndPost(canvas);
+            }
+        }
+    }
+}
