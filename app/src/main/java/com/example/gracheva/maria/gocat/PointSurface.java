e28ffe703524db3d9385bd81363f6854f44875ac
maria <marija.smolina@gmail.com>
2019-03-17 13:37:02 +0300

    add score calculating

--- app/src/main/java/com/example/gracheva/maria/gocat/PointSurface.java
+++ app/src/main/java/com/example/gracheva/maria/gocat/PointSurface.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -13,21 +15,33 @@ import com.example.gracheva.maria.gocat.animation.geometry.Point;
 import com.example.gracheva.maria.gocat.animation.randomFigure.RandomUnion;
 
 public class PointSurface extends SurfaceView {
+    private static final int POINT_WIDTH = 170;
+    private static final int POINT_HEIGHT = 170;
+    private static final int TEXT_SIZE = 60;
+    private static final String SCORE_MSG = "SCORE: ";
+
     volatile SurfaceHolder holder;
-    Paint paint;
-    Drawable wallpaper;
+    private Paint paint;
+    private Drawable wallpaper;
+    private Point currentPosition;
+    private Long score;
+    private Paint fontPaint;
 
     public PointSurface(Context context) {
         super(context);
         holder = getHolder();
         paint = new Paint();
         wallpaper = getResources().getDrawable(R.drawable.wallpaper, null);
+        currentPosition = new Point(-1000, -1000);
+        score = 0L;
+        fontPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        fontPaint.setTextSize(TEXT_SIZE);
+        fontPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        fontPaint.setColor(Color.GRAY);
         new PointAnimation().start();
     }
 
     private class PointAnimation extends Thread {
-        private static final int POINT_WIDTH = 170;
-        private static final int POINT_HEIGHT = 170;
 
         @Override
         public void run() {
@@ -53,8 +67,10 @@ public class PointSurface extends SurfaceView {
         private void draw(Point point) {
             Canvas canvas = holder.lockCanvas();
             if (canvas != null) {
+                currentPosition = point;
                 wallpaper.setBounds(0, 0, getWidth(), getHeight());
                 wallpaper.draw(canvas);
+                canvas.drawText(SCORE_MSG + score, 20, getHeight() - 20, fontPaint);
                 Bitmap bitmap = BitmapFactory.decodeResource(getContext().getResources(), R.drawable.point);
                 bitmap = Bitmap.createScaledBitmap(bitmap, POINT_WIDTH, POINT_HEIGHT, false);
                 canvas.drawBitmap(bitmap, (float) point.getX(), (float) point.getY(), paint);
@@ -65,5 +81,21 @@ public class PointSurface extends SurfaceView {
         private Point getScale() {
             return new Point(getWidth(), getHeight());
         }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                //Check if the x and y position of the touch is inside the point
+                if (x > currentPosition.getX() && x < currentPosition.getX() + POINT_WIDTH
+                        && y > currentPosition.getY() && y < currentPosition.getY() + POINT_HEIGHT) {
+                    score++;
+                }
+                return true;
+        }
+        return false;
     }
 }
